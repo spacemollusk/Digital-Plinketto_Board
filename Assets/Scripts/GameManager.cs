65fb3b658f9f0b60150d7d51060027b746a98dcb
Molly Carroll <72821176+spacemollusk@users.noreply.github.com>
2022-12-04 13:20:41 -0600

    gamemanager and ui update
    added TMP ui elements and connected game manager to them

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("UI Regerences")]
+    [SerializeField]
+    private TMP_Text playerPointsText;
+ 
+
+
+    private int playerPoints;
+    public int PlayerPoints
+    {
+        get
+        {
+            return playerPoints;
+        }
+        set
+        {
+            playerPoints = value;
+            playerPointsText.text = value.ToString();
+        }
+
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +35,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerPoints++;
+        }
     }
 }
